32359d7e1bc67527ff1faa9362694ff11048f4cb
sejungahn <ahnsj11@gmail.com>
2023-12-26 11:21:27 +0900

    [new] slot, provide/inject

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,9 @@ import HomeView from '../views/HomeView.vue'
 // import WatchEx from '../views/WatchEx' // watch
 // import axiosEx from '../views/DataBindingList2.vue'  // api server randering
 // import nestedComponent from '../views/NestedComponent.vue' // nested component
-import childComponent from '../views/ParentComponent5.vue'  // click event
+// import childComponent from '../views/ParentComponent5.vue'  // click event
+// import slotComponent from '../views/SlotUseModalLayout.vue'
+import provideinject from '../views/ProvideInject.vue'
  
 const routes = [
   {
@@ -41,7 +43,7 @@ const routes = [
   {
     path: '/databinding',
     name: 'DataBindingHtml',
-    component: childComponent
+    component: provideinject
   }
 ]
 
